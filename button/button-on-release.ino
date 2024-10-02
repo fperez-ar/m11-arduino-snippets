8bdf83b8182254827df6be988576ea3d939a7f8c
fperez-ar <34895680+fperez-ar@users.noreply.github.com>
2024-10-03 00:13:31 +0900

    Added btn base code

---
+++ button/button-on-release.ino
@@ -0,0 +1,27 @@
+#define HOW_MANY_BTNS 2
+const int pin_btn[HOW_MANY_BTNS] = {8, 9, 10};
+bool last_state[HOW_MANY_BTNS] = {false, false};
+void setup() {
+  Serial.begin(9600);
+  for (int i=0; i<HOW_MANY_BTNS; i++) {
+    pinMode(pin_btn[i], INPUT); // INPUT_PULLUP for no resistor per button
+  }
+}
+void loop() {
+  for (int i=0; i<HOW_MANY_BTNS; i++) {
+    int state = digitalRead(pin_btn[i]);
+    if (state != last_state[i]) {
+      last_state[i] = state;
+      if (state == LOW) {
+        // released 'event'
+        Serial.print("BTN Released: ");
+        Serial.print(pin_btn[i]);
+        Serial.print("-");
+        Serial.println(state);
+        // send signal, change display
+      }
+    }
+    // digitalWrite(pin_btn[i], HIGH);
+  }
+  delay(100);
+}
